bf0f869489f059e21e6f429dc78e0534d463a17c
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-08 01:04:01 +0530

    Home: default bookmarks removed

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,6 @@ import ErrorComponent from "components/ErrorComponent"
 
 import { createClient } from "utils/supabase/server"
 
-const defaultBookmarks = [
-  {
-    id: 1,
-    title: "Website",
-    link: "https://nirnejak.com",
-    icon: "https://nirnejak.com/favicon.ico",
-  },
-  {
-    id: 2,
-    title: "Twitter",
-    link: "https://twitter.com/jeetnirnejak",
-    icon: "https://twitter.com/favicon.ico",
-  },
-  {
-    id: 3,
-    title: "Layers.to",
-    link: "https://layers.to/nirnejak",
-    icon: "https://layers.to/build/favicon.ico",
-  },
-  {
-    id: 4,
-    title: "Github",
-    link: "https://github.com/nirnejak",
-    icon: "https://github.com/favicon.ico",
-  },
-]
-
 const Home = async (): Promise<React.JSX.Element> => {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
@@ -51,9 +24,7 @@ const Home = async (): Promise<React.JSX.Element> => {
     return (
       <main>
         <Bookmarks
-          defaultBookmarks={
-            bookmarks && bookmarks.length ? bookmarks : defaultBookmarks
-          }
+          defaultBookmarks={bookmarks && bookmarks.length ? bookmarks : []}
         />
         <Footer />
       </main>
