f12cd2bde74a5d1391fcd70d12eb2b19a3e21534
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-07 18:31:59 +0530

    Home: fetch bookmarks from supabase, removed dummy bookmarks

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,18 @@
 import * as React from "react"
+import { cookies } from "next/headers"
 
 import Bookmarks from "components/Bookmarks"
 import Footer from "components/Footer"
 
-export interface BOOKMARK {
-  id: number
-  title: string
-  link: string
-  icon: string
-}
-
-const getBookmarks = async (): Promise<BOOKMARK[]> => {
-  return [
-    { id: 1, title: "Website", link: "https://nirnejak.com", icon: "" },
-    {
-      id: 1,
-      title: "Dribbble",
-      link: "https://dribbble.com/nirnejak",
-      icon: "",
-    },
-    { id: 1, title: "Layers.to", link: "https://layers.to/nirnejak", icon: "" },
-    { id: 1, title: "Github", link: "https://github.com/nirnejak", icon: "" },
-  ]
-}
+import { createClient } from "utils/supabase/server"
 
 const Home = async (): Promise<React.JSX.Element> => {
-  const bookmarks = await getBookmarks()
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: bookmarks } = await supabase.from("bookmarks").select()
+
+  console.log(bookmarks)
 
   return (
     <main>
