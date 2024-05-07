f7d8fb558beae2b1b62accb8ac114e95a90f1455
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-07 18:20:07 +0530

    Get URL metadata to Favicon

---
+++ utils/getUrlFavicon.ts
@@ -0,0 +1,14 @@
+// import urlMetadata from "url-metadata"
+
+const getUrlFavicon = (url): string => {
+  // return await urlMetadata(url, {
+  //   requestHeaders: {
+  //     "User-Agent": "foo",
+  //     From: email,
+  //   },
+  // })
+  // TODO: extract domain from url and then add '/favicon.ico'
+  return ""
+}
+
+export default getUrlFavicon
