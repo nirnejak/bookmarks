4c12972085853566723cd7b6430f9e0ec8be097e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-07 18:33:55 +0530

    Home: dummy bookmarks if there's no data from supabase

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,18 @@ import Footer from "components/Footer"
 
 import { createClient } from "utils/supabase/server"
 
+const defaultBookmarks = [
+  { id: 1, title: "Website", link: "https://nirnejak.com", icon: "" },
+  {
+    id: 1,
+    title: "Dribbble",
+    link: "https://dribbble.com/nirnejak",
+    icon: "",
+  },
+  { id: 1, title: "Layers.to", link: "https://layers.to/nirnejak", icon: "" },
+  { id: 1, title: "Github", link: "https://github.com/nirnejak", icon: "" },
+]
+
 const Home = async (): Promise<React.JSX.Element> => {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore)
@@ -16,7 +28,7 @@ const Home = async (): Promise<React.JSX.Element> => {
 
   return (
     <main>
-      <Bookmarks defaultBookmarks={bookmarks} />
+      <Bookmarks defaultBookmarks={bookmarks || defaultBookmarks} />
       <Footer />
     </main>
   )
