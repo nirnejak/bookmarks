f3e9f494ff1d00156af4b100ed4d35729a4f7168
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-05-08 20:41:55 +0530

    ErrorComponent: refactor

--- components/ErrorComponent.tsx
+++ components/ErrorComponent.tsx
@@ -4,32 +4,36 @@ interface Props {
   type: number
 }
 
+interface IContent {
+  title: string
+  description: string
+}
+
 const ErrorComponent: React.FC<Props> = ({ type }) => {
-  switch (type) {
-    case 404:
-      return (
-        <div className="h-screen flex justify-center items-center">
-          <div className="text-center ">
-            <h1 className="mb-3 font-semibold text-slate-700">Not found</h1>
-            <p className="text-slate-500">
-              Page you're looking for does not exist.
-            </p>
-          </div>
-        </div>
-      )
-    case 500:
-    default:
-      return (
-        <div className="h-screen flex justify-center items-center">
-          <div className="text-center ">
-            <h1 className="mb-3 font-semibold text-slate-700">
-              Something went wrong
-            </h1>
-            <p className="text-slate-500">Please try again in some time.</p>
-          </div>
-        </div>
-      )
-  }
+  const content = React.useMemo<IContent>(() => {
+    switch (type) {
+      case 404:
+        return {
+          title: "Not found",
+          description: "Page you're looking for does not exist.",
+        }
+      case 500:
+      default:
+        return {
+          title: "Something went wrong",
+          description: "Please try again in some time.",
+        }
+    }
+  }, [type])
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-center ">
+        <h1 className="mb-3 font-semibold text-slate-700">{content.title}</h1>
+        <p className="text-slate-500">{content.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default ErrorComponent
